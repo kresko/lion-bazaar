6a89a0769971d9b32cb894381b4151972055eb8d
kresko <kresko1995@gmail.com>
2025-09-22 21:38:50 +0200

    Added console command for building category tree.

---
+++ src/Command/BuildCategoryTreeCommand.php
@@ -0,0 +1,77 @@
+<?php
+namespace App\Command;
+
+use App\Entity\Category;
+use App\Entity\CategoryTree;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: 'app:build-category-tree',
+    description: 'Builds JSON tree from categories and saves into category_tree table'
+)]
+class BuildCategoryTreeCommand extends Command
+{
+    public function __construct(private EntityManagerInterface $em)
+    {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $categoryRepository = $this->em->getRepository(Category::class);
+        $categories = $categoryRepository->findAll();
+
+        // Index categories by category_key for easy lookup
+        $categoriesByKey = [];
+        foreach ($categories as $category) {
+            $categoriesByKey[$category->getCategoryKey()] = [
+                'id_category' => $category->getId(),
+                'node_order' => $category->getNodeOrder(),
+                'category_key' => $category->getCategoryKey(),
+                'parent_category_key' => $category->getParentCategoryKey(),
+                'name' => $category->getName(),
+                'is_root' => $category->isRoot(),
+                'children' => [],
+            ];
+        }
+
+        // Attach children to parents
+        $tree = [];
+        foreach ($categoriesByKey as $key => &$category) {
+            if ($category['parent_category_key'] && isset($categoriesByKey[$category['parent_category_key']])) {
+                $categoriesByKey[$category['parent_category_key']]['children'][] = &$category;
+            } elseif ($category['is_root']) {
+                $tree[] = &$category;
+            }
+        }
+
+        // Sort children by node_order
+        $sortChildren = function (&$nodes) use (&$sortChildren) {
+            usort($nodes, fn($a, $b) => $a['node_order'] <=> $b['node_order']);
+            foreach ($nodes as &$node) {
+                if (!empty($node['children'])) {
+                    $sortChildren($node['children']);
+                }
+            }
+        };
+
+        $sortChildren($tree);
+
+        $categoryTree = $this->em->getRepository(CategoryTree::class)->findOneBy([]);
+        if (!$categoryTree) {
+            $categoryTree = new CategoryTree();
+        }
+        
+        $categoryTree->setTreeJson($tree);
+
+        $this->em->persist($categoryTree);
+        $this->em->flush();
+
+        $output->writeln('<info>Category tree built and saved successfully!</info>');
+        return Command::SUCCESS;
+    }
+}
