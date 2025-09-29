3e0aaef2c02192cf47de0d67289466186163c28b
kresko <kresko1995@gmail.com>
2025-09-29 20:11:52 +0200

    Added twig live components, added navigation.

---
+++ src/Repository/NavigationTreeRepository.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\NavigationTree;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class NavigationTreeRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, NavigationTree::class);
+    }
+
+    public function getTreeJson(): ?array
+    {
+        $navigationTree = $this->findOneBy([]);
+
+        if (!$navigationTree) {
+            return null;
+        }
+
+        $json = $navigationTree->getTreeJson();
+        
+        return is_string($json) ? json_decode($json, true) : $json;
+    }
+}
