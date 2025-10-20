cfac712825c27d9a57fa8981cdeb1edf9bdcec3c
kresko <kresko1995@gmail.com>
2025-10-20 21:04:58 +0200

    Removing comments.

--- src/Twig/Components/Footer/FooterNavigation.php
+++ src/Twig/Components/Footer/FooterNavigation.php
@@ -8,15 +8,20 @@ use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 #[AsTwigComponent]
 final class FooterNavigation
 {
+    /**
+     * @param CmsStorageRepository $cmsStorageRepository
+     */
     public function __construct(
         protected CmsStorageRepository $cmsStorageRepository
     )
     {
     }
 
+    /**
+     * @return array
+     */
     public function getFooterCmsNavigation(): array
     {
-        // napravi json
         $footerNavigationSlot = $this->cmsStorageRepository->findOneBy(['key' => 'footer_navigation_slot']);
 
         if (!$footerNavigationSlot) {
@@ -30,28 +35,29 @@ final class FooterNavigation
         return $navigation;
     }
 
+    /**
+     * @param array $slotData
+     * 
+     * @return array
+     */
     private function extractNavigationData(array $slotData): array
     {
         $navigation = [];
 
-        // Check if children exist in the slot
         if (!isset($slotData['children']) || !is_array($slotData['children'])) {
             return [];
         }
 
-        // Iterate through blocks
         foreach ($slotData['children'] as $block) {
             if ($block['type'] !== 'block' || !isset($block['children'])) {
                 continue;
             }
 
-            // Iterate through content items within each block
             foreach ($block['children'] as $contentItem) {
                 if ($contentItem['type'] !== 'content_item' || !isset($contentItem['data'])) {
                     continue;
                 }
 
-                // Transform the data format from [{name, url}] to [{label, url}]
                 $navigationColumn = array_map(function ($item) {
                     return [
                         'label' => $item['name'] ?? '',
